61e40fbb83b029c33d569d4129657a8cb84decbc
purva112006 <dhekanepurva@gmail.com>
2025-06-24 19:24:21 +0530

    Clean commit from purva112006

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import ContactPage from "./pages/ContactPage";
+import "./styles/pages.css";
+import AboutPage from "./pages/AboutPage";
+import CoursesPage from "./pages/CoursesPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import AdmissionsPage from "./pages/AdmissionPage";
+import ChatbotComponent from "./components/Chatbot/ChatbotComponents";
+
+function App() {
+  return (
+    <Router>
+      <div className="main-layout">
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/courses" element={<CoursesPage />} />
+            <Route path="/admissions" element={<AdmissionsPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+          <ChatbotComponent />
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+export default App;
+
